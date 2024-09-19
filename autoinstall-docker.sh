ac668236b078f4f23395e8b998e8912e9edccb80
DuncanZhang <duncan751122@gmail.com>
2024-09-19 13:51:47 +0800

    Update autoinstall-docker.sh

--- autoinstall-docker.sh
+++ autoinstall-docker.sh
@@ -13,9 +13,12 @@ echo -e "\033[34m[TASK 3] Download docker-compose && chmod\033[0m"
 sudo curl -L "https://github.com/docker/compose/releases/download/v2.24.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 
-echo -e "\033[34m[TASK 4] local user docker run access rights\033[0m"
+echo -e "\033[34m[TASK 4] Local user docker run access rights\033[0m"
 sudo groupadd docker
 USER=$(who | awk '{print $1}' | sort | uniq)
 sudo usermod -aG docker $USER
 
-echo -e "\033[34m====================done====================\033[0m"
+echo -e "\033[34m[TASK 4] Remove shell script files\033[0m"
+rm -f auto.sh get-docker.sh
+
+echo -e "\033[34m===============Automatic installation completed===============\033[0m"
